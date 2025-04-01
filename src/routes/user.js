53df997b87d9431e7231bb355288966bde2654bc
Sandra Rampal <sandrarampal@macbookpro.lan>
2025-04-01 16:24:43 +0200

    changed user route name to /api/users

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const UsersController = require("../controllers/user_controller");
 const asyncHandler = require("../middlewares/async-handler");
 
-router.get("/users", asyncHandler(UsersController.getUsers));
+router.get("/api/users", asyncHandler(UsersController.getUsers));
 
 module.exports = router;
