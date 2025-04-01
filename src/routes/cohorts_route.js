def83233b832b0de981b36005b21b9c59b527165
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-04-01 16:24:08 +0200

    added asyncHandler to cohort route

--- src/routes/cohorts_route.js
+++ src/routes/cohorts_route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const cohorts_controller = require("../controllers/cohorts_controller");
+const asyncHandler = require("../middlewares/async-handler");
 
-router.get("/", cohorts_controller.getAllCohorts);
-router.post("/", cohorts_controller.createCohort);
+router.get("/", asyncHandler(cohorts_controller.getAllCohorts));
+router.post("/", asyncHandler(cohorts_controller.createCohort));
 
 module.exports = router;
