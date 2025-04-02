457d527985da552509653fc4b7f83611775510b0
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-04-02 08:57:24 +0200

    clean cohorts_contollers after Nico check

--- src/controllers/cohorts_controller.js
+++ src/controllers/cohorts_controller.js
@@ -5,28 +5,11 @@ const getAllCohorts = async (req, res, next) => {
   //fecth all cohorts and get only _id and name field
   const allCohorts = await Cohort.find({}, "_id, name");
 
-  //if non cohort exist
-  if (allCohorts.length === 0) {
-    return next(new NotFoundError("No existing cohort"));
-  }
-
   res.status(200).json(allCohorts);
 };
 
 const createCohort = async (req, res, next) => {
   const { name } = req.body;
-
-  //check the parameter
-  if (!req.body.name) {
-    return next(new AppError("Missing required field: name"));
-  }
-
-  //check if cohort already exist
-  const isCohortExist = await Cohort.findOne({ name });
-
-  if (isCohortExist) {
-    return next(new ConflictError("The cohort already exist"));
-  }
 
   //create the new cohort and save it in DB
   const newCohort = new Cohort({ name });
