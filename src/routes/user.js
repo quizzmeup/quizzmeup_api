4681a3d69f1ce181295dae060ac07c13ad04a8f3
Nicolas Sekri <nicolassekri@gmail.com>
2025-04-02 13:44:04 +0200

    added adminOnly middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const UsersController = require("../controllers/user_controller");
 const asyncHandler = require("../middlewares/async-handler");
+const adminOnly = require("../middlewares/admin_only");
 
-router.get("/api/users", asyncHandler(UsersController.getUsers));
+router.get("/api/users", adminOnly, asyncHandler(UsersController.getUsers));
 
 module.exports = router;
