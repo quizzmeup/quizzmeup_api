6e13fd17e1bc3ab90f994628b7645cd3c73cd67c
Nicolas Sekri <nicolassekri@gmail.com>
2025-04-03 05:08:25 +0200

    protected cohorts and users routes with auth middleware

--- src/routes/cohorts_route.js
+++ src/routes/cohorts_route.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const cohorts_controller = require("../controllers/cohorts_controller");
 const asyncHandler = require("../middlewares/async-handler");
+const authMiddleware = require("../middlewares/auth_middleware");
 const adminOnly = require("../middlewares/admin_only");
+
+router.use(authMiddleware);
 
 router.get("/", asyncHandler(cohorts_controller.getAllCohorts));
 router.post("/", adminOnly, asyncHandler(cohorts_controller.createCohort));
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const UsersController = require("../controllers/user_controller");
 const asyncHandler = require("../middlewares/async-handler");
+const authMiddleware = require("../middlewares/auth_middleware");
 const adminOnly = require("../middlewares/admin_only");
+
+router.use(authMiddleware);
 
 router.get("/api/users", adminOnly, asyncHandler(UsersController.getUsers));
 
