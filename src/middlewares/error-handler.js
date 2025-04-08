03a5c94a590f97be9505b97cbcdf1fcad364f83b
Nicolas Sekri <nicolassekri@gmail.com>
2025-04-08 17:00:08 +0200

    polish

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -3,7 +3,6 @@ const { ConflictError, BadRequestError } = require("../utils/errors");
 const errorHandler = (err, req, res, next) => {
   // Gestion spécifique des erreurs mongodb d'unicité de champ
   if (err.code === 11000) {
-    console.log(err.code);
     const duplicatedField = Object.keys(err.keyValue || {})[0];
     const message = `Le champ "${duplicatedField}" est déjà utilisé.`;
     err = new ConflictError(message);
